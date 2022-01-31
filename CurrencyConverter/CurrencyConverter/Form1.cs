1dd346759a2377272dfc957039bc5b14cf270504
beytullah-gn <b-gnydn_@hotmail.com>
2022-01-31 13:24:20 +0300

    Main

---
+++ CurrencyConverter/CurrencyConverter/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CurrencyConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://www.google.com/search?client=opera-gx&q=1+ust+to+try&sourceid=opera&ie=UTF-8&oe=UTF-8");
+        }
+    }
+}
